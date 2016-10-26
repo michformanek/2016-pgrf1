913554d8fb91839fa14981f0a55d1c19de711dd9
JV <spa.vanek.honza@gmail.com>
2016-10-26 14:44:59 +0200

    Add RasterImage interface

---
+++ src/rasterdata/RasterImage.java
@@ -0,0 +1,48 @@
+package rasterdata;
+
+import java.util.Optional;
+
+import annotations.NotNull;
+
+public interface RasterImage<PixelType> {
+	/**
+	 * Returns value of the pixel at the given column and row address if valid,
+	 * empty Optional otherwise
+	 * 
+	 * @param c
+	 *            column address
+	 * @param r
+	 *            row address
+	 * @return pixel value, empty Optional if address is invalid, not null
+	 */
+	@NotNull Optional<PixelType> getPixel(int c, int r);
+
+	/**
+	 * Returns a RasterImage with the value of the pixel at the given column and
+	 * row address set to the given value
+	 * 
+	 * @param c
+	 *            column address
+	 * @param r
+	 *            row address
+	 * @param pixel
+	 *            value to be set to the pixel, must not be null
+	 * @return RasterImage with pixel set to given value, this if address is
+	 *         invalid
+	 */
+	@NotNull RasterImage<PixelType> withPixel(int c, int r, @NotNull PixelType pixel);
+
+	/**
+	 * Returns the number of rows of this raster image
+	 * 
+	 * @return number of rows
+	 */
+	int getHeight();
+
+	/**
+	 * Returns the number of columns of this raster image
+	 * 
+	 * @return number of columns
+	 */
+	int getWidth();
+}
