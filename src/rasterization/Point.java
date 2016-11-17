05fb9ed33a8c1208b00e2c3cb6d05e576e67f5fb
Michal Formanek <michformanek@gmail.com>
2016-11-17 20:55:26 +0100

    Change Point to double

---
+++ src/rasterization/Point.java
@@ -0,0 +1,22 @@
+package rasterization;
+
+/**
+ * Created by Michal Formanek on 17.11.16.
+ */
+final public class Point {
+
+	private final double x, y;
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+}
