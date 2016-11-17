52d4d50c0135a78c3309367bcf9c7357a09c41a8
Michal Formanek <michformanek@gmail.com>
2016-11-17 21:17:17 +0100

    Add PolygonRasterizer implementation

---
+++ src/rasterization/PolygonRasterizerImpl.java
@@ -0,0 +1,27 @@
+package rasterization;
+
+import annotations.NotNull;
+import rasterdata.RasterImage;
+
+import java.util.List;
+
+/**
+ * Created by Michal Formanek on 17.11.16.
+ */
+public class PolygonRasterizerImpl<PixelType> implements PolygonRasterizer<PixelType> {
+
+	@Override
+	@NotNull
+	public RasterImage<PixelType> drawPolygon(@NotNull final RasterImage<PixelType> img, @NotNull final List<Point> vertices,
+	                                          @NotNull final LineRasterizer<PixelType> liner, @NotNull final PixelType pixel) {
+		RasterImage<PixelType> result = img;
+		for (int i = 0; i < vertices.size(); i++) {
+			final double startX = vertices.get(i).getX();
+			final double startY = vertices.get(i).getX();
+			final double endX = vertices.get(i % vertices.size()).getX();
+			final double endY = vertices.get(i % vertices.size()).getY();
+			result = liner.drawLine(result, startX, startY, endX, endY, pixel);
+		}
+		return result;
+	}
+}
