bfd0a4397827b9953cda0b09a89a0c1b7a2638db
Michal Formanek <michformanek@gmail.com>
2016-11-18 14:33:22 +0100

    Add LineRasterizerFactory

---
+++ src/rasterization/LineRasterizerFactory.java
@@ -0,0 +1,22 @@
+package rasterization;
+
+/**
+ * Created by Michal Formanek on 18.11.16.
+ */
+public class LineRasterizerFactory<PixelType> {
+
+	/**
+	 * Returns new instance of LineRasterizer specified by given type.
+	 *
+	 * @param type Type of returned LineRasterizer
+	 * @return LineRasterizer of givet type
+	 */
+	public LineRasterizer<PixelType> getLineRasterizer(LineRasterizerType type) {
+		if (type == LineRasterizerType.TRIVIAL)
+			return new LineRasterizerTrivial<>();
+		else if (type == LineRasterizerType.DDA)
+			return new LineRasterizerDDA<>();
+		else
+			return new LineRasterizerDDA<>();
+	}
+}
